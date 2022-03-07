ffd5cfdf4f75bed1e2784065eb128d692cef25ab
Jaroslav Valdauf~ <jarda@valdauf.eu>
2022-03-07 14:09:08 +0100

    robots.txt

--- cayenne_decoder/urls.py
+++ cayenne_decoder/urls.py
@@ -1,3 +1,4 @@
+from django.views import generic
 from django.contrib import admin
 from django.urls import path
 
@@ -7,4 +8,5 @@ urlpatterns = [
     #path('admin/', admin.site.urls),
     path('', views.IndexView.as_view(), name='index'),
     path('decode', views.DecodeView.as_view(), name='decode'),
+    path("robots.txt", generic.TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
 ]
